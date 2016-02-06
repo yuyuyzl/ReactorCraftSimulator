c4f0b9376fa1f081284c5c27bbdf3ef745f77d4c
yuyuyzl <yuzhongliang22@163.COM>
2016-02-06 10:42:04 +0800

    more tests

--- javascript/app.js
+++ javascript/app.js
@@ -3,9 +3,15 @@
  */
 (function(){
     var app=angular.module("main",[]);
+
     app.controller("mainController",function(){
         this.col=2;
         this.row=3;
+
+        this.showalert=function(s){
+            alert(s)
+        }
+
         this.fun=function(n){
             var ans=[];
             for (var i=0;i<n;i++){
