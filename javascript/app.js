6d7c19519295f47ed750c2d0dc79f2299eaba076
yuyuyzl <yuzhongliang22@163.COM>
2016-02-15 01:11:00 +0800

    啊哈哈搞定啦！

--- javascript/app.js
+++ javascript/app.js
@@ -9,11 +9,13 @@
         this.col=8;
         this.row=8;
         this.chosen=1;
-
+        this.maxtime=30000000;
+        this.steptime=100000;
+        this.stopNow=false;
         this.clearData=function(){
             this.data=[];
             for (var i=0;i<32;i++)this.data.push(new Array(32));
-        }
+        };
         this.clearData();
         this.getRangeArray=function(n){
             var ans=[];
@@ -31,19 +33,20 @@
         this.blockNames=["Air","Fuel Rod","Steam Boiler","Reflector","Steel Block"];
         this.blockImg=["img/blank.png","img/fuel_top.png","img/boiler.png","img/reflector.png","img/steel.png"];
         this.blockValue=[null,Block.Type.CORE,Block.Type.BOILER,Block.Type.REFLECTOR,Block.Type.STEEL];
-        //*************从这里开始往下看@imbushuo********************
-        this.outputHtml=function(s){//这个函数输出到右下角和更新绑定的outputFieldSafe域
+
+        this.outputHtml=function(s){
             console.log(s);
             $scope.outputFieldSafe=$sce.trustAsHtml(s);
-        }
+
+        };
         this.outputHtml("No output.<br>Run an emulation now!<br>Let's Rock!");
-        this.emulate=function(){    //主要的模拟过程，现在只有温度传导可用
-            //var deferred=$q.defer();
+        this.emulate=function(){
+            this.stopNow=false;
             var a=this;
-            //$timeout(function(){
 
 
-                var wd=RecWorld.createNew(Math.max(a.col,a.row));//下面是根据data数组生成世界
+
+                var wd=RecWorld.createNew(Math.max(a.col,a.row));
                 //console.log(this.data.length)
                 for (var i=0;i<a.data.length;i++){
                     //console.log(this.data[i].length)
@@ -51,22 +54,44 @@
                         //console.log(i+" "+j);
                         if (a.data[i][j]!=null)wd.setBlock(a.blockValue[a.data[i][j]],i,j);
                     }
-                }//上面是根据data数组生成世界，这一步不占用时间
-
-
-                //主要耗时过程
-                for (var i=0;i<3000000;i++){
-                    wd.doTick();//实际上每一次doTick都是相对很短时间的，但是要做相当大量才能得出结果
-                    if (wd.worldTick%1000000==0)this.outputHtml(wd.printWorld())//deferred.notify(wd.printWorld());//希望经过指定更新步数或者指定时间后更新一次状态栏
-                    wd.worldTick++;
                 }
-            this.outputHtml(wd.printWorld())//deferred.resolve(wd.printWorld());结束后最后更新一次状态栏
-            //},0);
-            //return deferred.promise;
+
+                var emdefer=function(){
+                    var deferred=$q.defer();
+                    setTimeout(function(){
+                        for (var i=0;i< a.steptime;i++){
+                            wd.doTick();
+                            wd.worldTick++;
+
+                        }
+                        deferred.resolve();
+                    },0);
+                    return deferred.promise;
+                };
+
+
+
+                        function dofun(rmtime) {
+                            emdefer().then(function () {
+                                a.outputHtml(wd.printWorld());
+                                if (!a.stopNow && rmtime-a.steptime>0)dofun(rmtime- a.steptime);
+                            });
+                        }
+
+                dofun(a.maxtime);
+
+
+
+
+
+
+
+
+
 
 
 
         }
     }]);
-////*************看到这里就可以了@imbushuo********************
+
 })();
